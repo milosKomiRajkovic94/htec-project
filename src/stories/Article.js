a0f5c7c59f287e6592b7466d4b3f995d28695e71
milosKomiRajkovic94 <rajkovicm94@gmail.com>
2020-09-21 18:34:52 +0200

    refactor (upgrade): Several code improvements as well as change in README.md

--- src/stories/Article.js
+++ src/stories/Article.js
@@ -1,51 +1,97 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 import PropTypes from "prop-types";
 
 import "./article.css";
 
 export const Article = ({ singleArticleData, onMore }) => {
+  const [popUpDialogWithFullTitle, setPopUpDialogWithFullTitle] = useState(
+    false
+  );
+  const titleRef = useRef(null);
+
+  function onMouseOverOrLeave() {
+    setPopUpDialogWithFullTitle(!popUpDialogWithFullTitle);
+  }
+
+  function onFocus() {
+    titleRef.focus();
+  }
+
   return (
-    <div className="singleArticle">
+    <div className="articleWrapper">
       <div
         className={
-          singleArticleData.title
-            ? "singleArticleTitle"
-            : "singleArticleTitle visibilityHidden"
+          popUpDialogWithFullTitle
+            ? "animated fadeIn singleArticleFullTitle"
+            : "displayNone"
+        }
+        style={
+          document.getElementById("originalTitle")
+            ? {
+                top: `${
+                  document
+                    .getElementById("originalTitle")
+                    .getBoundingClientRect().top + window.scrollY
+                }px`,
+                left: `${
+                  document
+                    .getElementById("originalTitle")
+                    .getBoundingClientRect().left + 260
+                }px`,
+              }
+            : {}
         }
       >
         {singleArticleData.title}
       </div>
-      <div className="singleArticleImage">
-        <img
-          src={
-            singleArticleData.urlToImage
-              ? singleArticleData.urlToImage
-              : require("../assets/images/boxImage.png")
+      <div className="singleArticle">
+        <div
+          className={
+            singleArticleData.title
+              ? "singleArticleTitle"
+              : "singleArticleTitle visibilityHidden"
           }
-          className="singleArticleImageTag"
-          alt="singleArticleImageTag"
-        />
-      </div>
-      <div
-        className={
-          singleArticleData.description
-            ? "singleArticleDescription"
-            : "singleArticleDescription visibilityHidden"
-        }
-      >
-        {singleArticleData.description}
-      </div>
-      <div className="singleArticleMoreOption">
-        <span
-          className="cursorPointer"
-          role="button"
-          onClick={() => onMore()}
-          onKeyPress={() => onMore()}
-          tabIndex={0}
+          onMouseOver={onMouseOverOrLeave}
+          onMouseLeave={onMouseOverOrLeave}
+          onFocus={onFocus}
+          role="banner"
+          id="originalTitle"
+          ref={titleRef}
         >
-          {"More >"}
-        </span>
+          {singleArticleData.title}
+        </div>
+        <div className="singleArticleImage">
+          <img
+            src={
+              singleArticleData.urlToImage
+                ? singleArticleData.urlToImage
+                : require("../assets/images/boxImage.png")
+            }
+            className="singleArticleImageTag"
+            alt="singleArticleImageTag"
+          />
+        </div>
+        <div
+          className={
+            singleArticleData.description
+              ? "singleArticleDescription"
+              : "singleArticleDescription visibilityHidden"
+          }
+        >
+          {singleArticleData.description}
+        </div>
+        <div className="singleArticleMoreOption">
+          <span
+            className="cursorPointer"
+            role="button"
+            onClick={() => onMore()}
+            onKeyPress={() => onMore()}
+            tabIndex={0}
+          >
+            {"More >"}
+          </span>
+        </div>
       </div>
     </div>
   );
